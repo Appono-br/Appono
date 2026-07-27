b8bcc54add89c07b7685e36774eb8fce6f7f4ceb
Appono-br <appono.br@gmail.com>
2026-07-27 16:54:18 -0300

    Padroniza estetica dos modulos

---
+++ frontend/app/admin/layout.jsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { useSyncExternalStore } from "react";
+import { useTemaLocal } from "@/lib/use-tema-local";
+import { TelaCarregandoSessao } from "@/lib/use-sessao-local";
+
+function inscrever() {
+    return () => { };
+}
+
+function obterEstadoCliente() {
+    return true;
+}
+
+function obterEstadoServidor() {
+    return false;
+}
+
+export default function LayoutAdmin({ children }) {
+    const { tema } = useTemaLocal();
+    const estaNoNavegador = useSyncExternalStore(inscrever, obterEstadoCliente, obterEstadoServidor);
+
+    if (!estaNoNavegador) {
+        return <TelaCarregandoSessao />;
+    }
+
+    return (
+        <div className={`area-autenticada min-h-full ${tema === "escuro" ? "tema-escuro" : ""}`}>
+            {children}
+        </div>
+    );
+}
